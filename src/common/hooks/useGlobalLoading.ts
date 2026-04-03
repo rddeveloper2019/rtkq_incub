37102b44ffe727f518fb093941351eaed07690fb
Ridvan Dzh <63258916+rddeveloper2019@users.noreply.github.com>
2026-04-03 22:05:57 +0300

    useGlobalLoading

--- src/common/hooks/useGlobalLoading.ts
+++ src/common/hooks/useGlobalLoading.ts
@@ -1,15 +1,30 @@
+import { playlistsApi } from '@/features/playlists/api/playlistsApi';
+import { tracksApi } from '@/features/tracks/api/tracksApi';
 import type { RootState } from '@/app/model/store';
 import { useSelector } from 'react-redux';
+
+// Список эндпоинтов для исключения из глобального индикатора
+const excludedEndpoints = [
+  playlistsApi.endpoints.fetchPlaylists.name,
+  tracksApi.endpoints.fetchTracks.name,
+];
 
 export const useGlobalLoading = () => {
   return useSelector((state: RootState) => {
     // Получаем все активные запросы из RTK Query API
     const queries = Object.values(state.baseApi.queries || {});
     const mutations = Object.values(state.baseApi.mutations || {});
-    // Проверяем, есть ли активные запросы (статус 'pending')
-    const hasActiveQueries = queries.some(
-      (query) => query?.status === 'pending',
-    );
+
+    const hasActiveQueries = queries.some((query) => {
+      if (query?.status !== 'pending') return;
+      if (excludedEndpoints.includes(query.endpointName)) {
+        const completedQueries = queries.filter(
+          (q) => q?.status === 'fulfilled',
+        );
+        return completedQueries.length > 0;
+      }
+    });
+
     const hasActiveMutations = mutations.some(
       (mutation) => mutation?.status === 'pending',
     );
